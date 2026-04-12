c77f5601c035fc07dd1cbc0cfb942ac6148c1a7d
aidilsfflbhrn <aidil90210@gmail.com>
2026-04-12 18:49:07 +0800

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,12 +1,4 @@
-import { createServer } from "http";
-
-export const config = {
-  api: {
-    bodyParser: true,
-  },
-};
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -29,3 +21,4 @@ export default async function handler(req, res) {
   } catch(e) {
     return res.status(500).json({ error: e.message });
   }
+};
